d645b1249984bae596cc0be2b44c7b3af896b873
mweerts <maximeweerts@hotmail.be>
2019-11-28 17:36:12 +0100

    include stdarg

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 20:14:55 by mweerts           #+#    #+#             */
-/*   Updated: 2019/11/27 15:31:28 by mweerts          ###   ########.fr       */
+/*   Updated: 2019/11/28 17:35:58 by mweerts          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFTPRINTF_H
 
 # include "./libft/libft.h"
+# include <stdarg.h>
 
 int	ft_printf(const char *str, ...);
 
