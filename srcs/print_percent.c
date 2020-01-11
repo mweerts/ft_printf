f52dccf1dc6ae51a748cfb77d7afe2afca98da36
mweerts <mweerts@student.s19.be>
2020-01-11 14:30:56 -0800

    modif percent

--- srcs/print_percent.c
+++ srcs/print_percent.c
@@ -6,14 +6,29 @@
 /*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/05 10:01:50 by mweerts           #+#    #+#             */
-/*   Updated: 2020/01/11 09:33:03 by mweerts          ###   ########.fr       */
+/*   Updated: 2020/01/11 14:30:47 by mweerts          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libftprintf.h"
 
-int	print_percent(void)
+int	print_percent(t_flag *flag)
 {
-	write(1, "%", 1);
+	int	count;
+
+	count = 1;
+	if (flag->minus)
+		write(1, "%", 1);
+	while (flag->width > 1)
+	{
+		if (flag->zero)
+			write(1, "0", 1);
+		else
+			write(1, " ", 1);
+		flag->width--;
+		count++;
+	}
+	if (!flag->minus)
+		write(1, "%", 1);
 	return (1);
 }
