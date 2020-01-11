934af0b4b9fc787cf3fa672ff339ae502a18c047
mweerts <mweerts@student.s19.be>
2020-01-11 13:08:56 -0800

    modif

--- srcs/print_string.c
+++ srcs/print_string.c
@@ -6,7 +6,7 @@
 /*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/05 06:01:34 by mweerts           #+#    #+#             */
-/*   Updated: 2020/01/11 13:04:13 by mweerts          ###   ########.fr       */
+/*   Updated: 2020/01/11 13:08:38 by mweerts          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,20 +32,17 @@ int	print_string(va_list ap, t_flag *flag)
 	count = 0;
 	str = va_arg(ap, char*);
 	if (str == NULL)
-		len = 6;
-	else if (flag->precision != -1 && (int)ft_strlen(str) > flag->precision)
+		str = ft_strdup("(null)");
+	if (flag->precision != -1 && (int)ft_strlen(str) > flag->precision)
 		len = flag->precision;
 	else
 		len = ft_strlen(str);
 	if (flag->minus)
 	{
-		if (str)
 			count += ft_putstr_len(str, flag->precision);
-		else
-			count += ft_putstr_len("(null)", flag->precision);
 	}
 	while (flag->width > len)
-	{
+ 	{
 		if (flag->zero && !flag->minus)
 			write(1, "0", 1);
 		else
@@ -55,10 +52,7 @@ int	print_string(va_list ap, t_flag *flag)
 	}
 	if (!flag->minus)
 	{
-		if (str)
 			count += ft_putstr_len(str, flag->precision);
-		else
-			count += ft_putstr_len("(null)", flag->precision);
 	}
 	return (count);
 }
