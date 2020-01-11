c9886e32ae99d34d7504cacb107fdd9cc8b5b9a3
mweerts <mweerts@student.s19.be>
2020-01-11 14:44:30 -0800

    modif percent

--- srcs/print_percent.c
+++ srcs/print_percent.c
@@ -6,7 +6,7 @@
 /*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/05 10:01:50 by mweerts           #+#    #+#             */
-/*   Updated: 2020/01/11 14:42:23 by mweerts          ###   ########.fr       */
+/*   Updated: 2020/01/11 14:44:03 by mweerts          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	print_percent(t_flag *flag)
 		write(1, "%", 1);
 	while (flag->width > 1)
 	{
-		if (flag->zero)
+		if (flag->zero && !flag->minus)
 			write(1, "0", 1);
 		else
 			write(1, " ", 1);
