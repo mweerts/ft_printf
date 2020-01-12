8c4a327a4add54a83374209416653522dd4956a2
mweerts <mweerts@student.s19.be>
2020-01-12 08:46:07 -0800

    modif char

--- srcs/print_char.c
+++ srcs/print_char.c
@@ -6,7 +6,7 @@
 /*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/05 05:46:57 by mweerts           #+#    #+#             */
-/*   Updated: 2020/01/11 14:34:50 by mweerts          ###   ########.fr       */
+/*   Updated: 2020/01/12 08:45:49 by mweerts          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,9 @@ int	print_char(va_list ap, t_flag *flag)
 	count = 1;
 	if (flag->minus)
 		write(1, &c, 1);
-	while (flag->width > 1)
+	while (flag->width > count)
 	{
 		write(1, " ", 1);
-		flag->width--;
 		count++;
 	}
 	if (!flag->minus)
