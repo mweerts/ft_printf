33c69c7772e206ce84754e248257696cdc60b316
mweerts <mweerts@student.s19.be>
2020-01-13 13:44:54 -0800

    modif

--- srcs/print_uint.c
+++ srcs/print_uint.c
@@ -6,17 +6,51 @@
 /*   By: mweerts <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/05 10:21:19 by mweerts           #+#    #+#             */
-/*   Updated: 2020/01/13 13:31:27 by mweerts          ###   ########.fr       */
+/*   Updated: 2020/01/13 13:44:32 by mweerts          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libftprintf.h"
 
+static	t_number	get_number(va_list ap, t_flag *flag)
+{
+    t_number		number;
+    long	long	nbr;
+
+    number.len = 0;
+	nbr = 0;
+    if (flag->l)
+        nbr = (long long)va_arg(ap, unsigned long int);
+	else if (flag->ll)
+	    nbr = va_arg(ap, unsigned long long int);
+	else if (flag->h)
+	    nbr = (long long)(unsigned short int)va_arg(ap, int);
+	else if (flag->hh)
+	    nbr = (long long)(unsigned char)va_arg(ap, int);
+	else	
+	    nbr = (long long)va_arg(ap, unsigned int);
+    number.sign = '+';
+	ft_longlongtoa(nbr, &number, flag->precision);
+    return (number);
+}
+
 int	print_uint(va_list ap, t_flag *flag)
 {
-	unsigned int	nbr;
+	t_number	nbr;
+	int			count;
 
-	flag->precision = 1;
-	nbr = va_arg(ap, unsigned int);
-	return (ft_putstr(ft_uitoa(nbr)));
+	nbr = get_number(ap, flag);
+	count = nbr.len;
+	if (flag->minus)
+		ft_putstr(nbr.str);
+	while (flag->width > count)
+ 	{
+		if (flag->zero && !flag->minus)
+			count += ft_putchar('0');
+		else
+			count += ft_putchar(' ');
+	}
+	if (!flag->minus)
+		ft_putstr(nbr.str);
+	return (count);
 }
